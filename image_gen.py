cb0f9361f22f7ed43ba8dd69ed341db5237893d2
Kuina <75318520+Kuina-chan@users.noreply.github.com>
2024-10-07 02:49:39 +0700

    move to use StreamCompanion to extract all the data

--- image_gen.py
+++ image_gen.py
@@ -6,42 +6,42 @@ from osrparse import Replay
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 from roundcorner import add_rounded_corners
-#extract data from local replay
-
-r = Replay.from_path("./replays/[Bau] - Hatsune Miku - Atama no Taisou [AngelHoney] (2021-12-15) Osu.osr")
-
-beatmap_hash = r.beatmap_hash
-player = r.username
-play_maxcombo = str(r.max_combo)
-c300 = r.count_300
-c100 = r.count_100
-c50 = r.count_50
-c0 = r.count_miss
-username = r.username
-play_accuracy = (300*(c300) + 100*(c100) + 50*(c50)) / (300*(c300 + c100 + c50 + c0))
-
+import json
 #extract data from beatmap online
 load_dotenv()
 userAPI: Final[str] = os.getenv("osu_API")
-url = f"https://osu.ppy.sh/api/get_beatmaps?k={userAPI}&h={beatmap_hash}"
 
-beatmap_data = requests.get(url)
-if beatmap_data.status_code == 200:
-    bdata = beatmap_data.json()
-    beatmap_name = bdata[0]['title']
-    max_combo = str(bdata[0]['max_combo'])
-    diff_name = bdata[0]['version']
-    circle_size = bdata[0]['diff_size']
-    overall_diff = bdata[0]['diff_overall']
-    approach_rate = bdata[0]['diff_approach']
-    drain_HP = bdata[0]['diff_drain']
-    beatmap_BPM = bdata[0]['bpm']
-    mapper = bdata[0]['creator']
-    star_rating = float(bdata[0]['difficultyrating'])
-    map_status = bdata[0]['approved']
-else: 
-    print(f"There is something wrong, probably peppy got dunked")
+mapData = requests.get(url="http://localhost:20727/json")
+if not mapData.status_code == 200:
+    print(f"Please check if either osu! is running or StreamCompanion is running")
+else:
+    data = json.loads(mapData.content.decode("utf-8-sig"))
+    max_combo = data["maxCombo"]
+    circle_size = data["mCS"]
+    overall_diff = data["mOD"]
+    approach_rate = data['mAR']
+    drain_HP = data["mHP"]
+    beatmap_BPM = data["mMainBpm"]
+    star_rating = data["mStars"]
+    mapper = data["creator"]
+    play_accuracy = data["acc"]
+    username = data["username"]
+    beatmap_hash = data["md5"]
+    play_maxcombo = data["currentMaxCombo"]
+    c300 = data["c300"]
+    c100 = data["c100"]
+    c50 = data["c50"]
+    c0 = data["miss"]
+    playPp = data["ppIfMapEndsNow"]
+    mods = data["mods"]
+    titleUnicode = data["titleUnicode"]
+    diff_name = data["diffName"]
+    grade = data["grade"]
 
+if approach_rate >= 10:
+    approach_rate = 10
+if overall_diff >= 10:
+    overall_diff = 10
 #extract player avatar
 if not os.path.exists(f'./player/{username}.png'):
     user = f"https://osu.ppy.sh/api/get_user?k={userAPI}&u={username}"
@@ -70,17 +70,26 @@ else:
     print(f'Existed an image at ./player/{username}.png')
 
 #Mod flags
-NoMod       =  0
-NoFail      =  1 << 0
-Easy        =  1 << 1
-Hidden      =  1 << 3
-HardRock    =  1 << 4
-SuddenDeath =  1 << 5
-DoubleTime  =  1 << 6
-Relax       =  1 << 7
-HalfTime    =  1 << 8
-Nightcore   =  1 << 9
-Flashlight  =  1 << 10
+NoMod       =  "None"
+NoFail      =  "NF"
+Easy        =  "EZ"
+Hidden      =  "HD"
+HardRock    =  "HR"
+SuddenDeath =  "SD"
+DoubleTime  =  "DT"
+Relax       =  "RX"
+HalfTime    =  "HT"
+Nightcore   =  "NC"
+Flashlight  =  "FL"
+
+url = f"https://osu.ppy.sh/api/get_beatmaps?k={userAPI}&h={beatmap_hash}"
+
+beatmap_data = requests.get(url)
+if beatmap_data.status_code == 200:
+    bdata = beatmap_data.json()
+    map_status = bdata[0]['approved']
+else: 
+    print(f"There is something wrong, probably peppy got dunked")
 
 #creating the image
 background = Image.open("./deps/bone.png")
@@ -91,14 +100,12 @@ font_path = "./fonts/BEBASNEUE-REGULAR.TTF"
 playcombo = f"{play_maxcombo}/{max_combo}X"
 
 texts = [
-    {"type": "player", "text": f"{player}", "position": [244, 47.74], "font_size": 138.58},
+    {"type": "player", "text": f"{username}", "position": [244, 47.74], "font_size": 138.58},
     {"type": "playcombo", "text": f"{playcombo}", "position": (48.5, 968.8), "font_size": 70},
-    {"type": "play_accuracy", "text": f"{play_accuracy:.2%}","position": [900, 951.8], "font_size": 90},
-    {"type": "circle_size", "text": f"CS: {circle_size}", "position": [1774.41, 642], "font_size": 61.34},
-    {"type": "overall_diff", "text": f"OD: {overall_diff}","position": [1774.41, 537.45], "font_size": 61.34},
-    {"type": "approach_rate", "text": f"AR: {approach_rate}","position": [1774.41, 329.3], "font_size": 61.34},
-    {"type": "drain_HP", "text": f"HP: {drain_HP}", "position": [1774.41, 748.3], "font_size": 61.34},
-    {"type": "beatmap_BPM", "text": f"{beatmap_BPM}BPM", "position": [1765.51, 15], "font_size": 47},
+    {"type": "play_accuracy", "text": f"{play_accuracy:.2f}%","position": [900, 951.8], "font_size": 90},
+    {"type": "circle_size", "text": f"CS: {circle_size:.1f}", "position": [1774.41, 642], "font_size": 61.34},
+    {"type": "drain_HP", "text": f"HP: {drain_HP:.1f}", "position": [1774.41, 748.3], "font_size": 61.34},
+    {"type": "beatmap_BPM", "text": f"{int(beatmap_BPM)}BPM", "position": [1765.51, 15], "font_size": 47},
     {"type": "mapper", "text": f"{mapper}", "position": [1575.1, 95.1], "font_size": 47},
     {"type": "star_rating", "text": f"{star_rating:.2f}*", "position": [1774.41, 433.46], "font_size": 61.34},
     {"type": "c300", "text": f"{str(c300)}x", "position": [26.39, 388.27], "font_size": 69.57},
@@ -122,40 +129,49 @@ if play_accuracy == 1.0:
         if item['type'] == "play_accuracy":
             item['position'] = [875, 951.8]
 
+if approach_rate >= 10:
+    approach_rate = 10
+    texts.append({"type": "approach_rate", "text": f"AR: {int(approach_rate)}","position": [1774.41, 329.3], "font_size": 61.34})
+else: 
+    texts.append({"type": "approach_rate", "text": f"AR: {approach_rate:.1f}","position": [1774.41, 329.3], "font_size": 61.34},)
+if overall_diff >= 10:
+    overall_diff = 10
+    texts.append({"type": "overall_diff", "text": f"OD: {overall_diff}","position": [1774.41, 537.45], "font_size": 61.34})
+else:
+    {"type": "overall_diff", "text": f"OD: {overall_diff:.1f}","position": [1774.41, 537.45], "font_size": 61.34},
+
+#handling pp
+formattedPp = int(playPp)
+if formattedPp < 1000:
+    texts.append({"type": "pp", "text": f"{formattedPp}pp", "position": [400, 850], "font_size": 201})
+elif formattedPp > 1000:
+    texts.append({"type": "pp", "text": f"{formattedPp}pp", "position": [360, 860], "font_size": 195})
+
 #handling grade
 grade_pos = [325, 190]
 
-percent_50 = c50 / (c300 + c100 + c50 +  c0)
-c300_percent = c300 / (c300 + c100 + c50 +  c0)
-if (play_accuracy == 1):
-    if (r.mods & Hidden or r.mods & Flashlight):
-        XH_grade = Image.open('./statics/ranking-XH.png')
-        background.paste(XH_grade, grade_pos, XH_grade)
-    else:
-        SS_grade = Image.open('./statics/ranking-X.png')
-        background.paste(SS_grade, grade_pos, SS_grade)
-
-elif (c300_percent < 1 and c300_percent > 0.9 and percent_50 <= 0.01 and c0 == 0):
-    if (r.mods & Hidden or r.mods & Flashlight):
-        SH_grade = Image.open('./statics/ranking-SH.png')
-        background.paste(SH_grade, grade_pos, SH_grade)
-    else:
-        S_grade = Image.open('./statics/ranking-S.png')
-        background.paste(S_grade, grade_pos, S_grade)
-
-elif ((c300_percent > 0.8 and c300_percent <= 0.9 and c0 == 0) or (c300_percent > 0.9 and c0 > 0)):
+if grade == 0:
+    XH_grade = Image.open('./statics/ranking-XH.png')
+    background.paste(XH_grade, grade_pos, XH_grade)
+elif grade == 2:
+    SS_grade = Image.open('./statics/ranking-X.png')
+    background.paste(SS_grade, grade_pos, SS_grade)
+elif grade == 1:
+    SH_grade = Image.open('./statics/ranking-SH.png')
+    background.paste(SH_grade, grade_pos, SH_grade)
+elif grade == 3:
+    S_grade = Image.open('./statics/ranking-S.png')
+    background.paste(S_grade, grade_pos, S_grade)
+elif grade == 4:
     A_grade = Image.open('./statics/ranking-A.png')
-    background.paste(A_grade, grade_pos, A_grade)
-
-elif ((c300_percent > 0.7 and c300_percent <= 0.8 and c0 == 0) or (c300_percent > 0.8 and c300_percent <= 0.9 and c0 > 0)):
+    background.paste(A_grade, grade_pos, A_grade)  
+elif grade == 5:
     B_grade = Image.open('./statics/ranking-B.png')
     background.paste(B_grade, grade_pos, B_grade)
-
-elif (c300_percent > 0.6 and c300_percent <= 0.7):
+elif (grade == 6):
     C_grade = Image.open('./statics/ranking-C.png')
     background.paste(C_grade, grade_pos, C_grade)
-
-else:
+elif grade == 7:
     D_grade = Image.open('./statics/ranking-D.png')
     background.paste(D_grade, grade_pos, D_grade)
 
@@ -227,7 +243,7 @@ elif map_status == "4":
 playAvatar = Image.open(f'./player/{username}.png')
 background.paste(playAvatar, [35, 15], playAvatar)
 
-mods = {
+mods_image = {
     NoFail: Image.open('./statics/nf.png'),
     DoubleTime: Image.open('./statics/dt.png'),
     Easy: Image.open('./statics/ez.png'),
@@ -237,17 +253,15 @@ mods = {
     HalfTime: Image.open('./statics/ht.png'),
     Nightcore: Image.open('./statics/nc.png')
 }
-
-mod_flags = r.mods.value
-
-mod_selection = [mods[mod] for mod in mods if mod_flags & mod]
-
 mod_pos = [(800, 730), (840, 730), (880, 730), (920, 730)]
 
-for i, mod_img in enumerate(mod_selection):
-    if i < len(mod_pos):
-        background.paste(mod_img, mod_pos[i], mod_img)
+selected_mods = [mod.strip() for mod in mods.split(',')]
 
+for i, mod_name in enumerate(selected_mods):
+    if mod_name in mods_image:
+        mod_img = mods_image[mod_name]
+        if i < len(mod_pos):
+            background.paste(mod_img, mod_pos[i], mod_img)
 #getting all da stuff on screen
 for item in texts:
     font = ImageFont.truetype(font_path, item["font_size"])
@@ -255,4 +269,5 @@ for item in texts:
     print(", ".join('{}: {}'.format(key, val) for key, val in item.items()))
 
 #fuck the background, why there is no function to define the layer for each item.
-background.save(f"./tests/{player} on {beatmap_name} [{diff_name}].png")
+background.save(f"./tests/{username} on {titleUnicode} [{diff_name}].png")
+#stop_application("tosu.exe")
