0a30006aa9b287ddc94a4c3a5fa08f9929090e71
Kuina <75318520+Kuina-chan@users.noreply.github.com>
2025-11-17 17:13:15 +0700

    Add: Log Levels

--- image_gen.py
+++ image_gen.py
@@ -12,6 +12,26 @@ import sys
 import re
 import numpy as np
 import shutil
+import logging as l
+
+logger = l.getLogger()
+
+handler = l.StreamHandler()
+
+log_level_str = os.environ.get('LOG_LEVEL', 'INFO').upper()
+log_level = getattr(l, log_level_str, l.INFO)
+
+handler.setLevel(log_level)
+
+formatter = l.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter.default_time_format = '%H:%M:%S'
+formatter.default_msec_format = '%s.%03d'
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+logger.setLevel(l.DEBUG)
+
 # Track the status of each required program
 tosu_running = False
 osu_running = False
@@ -41,11 +61,11 @@ if not (tosu_running and osu_running):
     if not osu_running:
         missing_programs.append("osu!.exe")
     
-    print(f"The following required programs are not detected: {', '.join(missing_programs)}! Quitting...")
+    l.warning(f"The following required programs are not detected: {', '.join(missing_programs)}! Quitting...")
     time.sleep(2)
     sys.exit()
 else:
-    print("Both tosu.exe and osu!.exe are detected. Continuing...")
+    l.info("Both tosu.exe and osu!.exe are detected. Continuing...")
   
 #extract data from beatmap using tosu
 load_dotenv()
@@ -54,7 +74,10 @@ userAPI: Final[str] = os.getenv("osu_API")
 mapData = requests.get(url="http://localhost:24050/json")
 
 if not mapData.status_code == 200:
-    print(f"Please check if either osu! is running or tosu is running correctly.")
+    l.error(f"Please check if either osu! is running or tosu is running correctly.")
+    l.info("Quitting...")
+    time.sleep(2)
+    sys.exit()
 else:
     data = json.loads(mapData.content)
     #background related data:
@@ -92,16 +115,16 @@ sanitized_title = invalid_chars_pattern.sub('_', titleUnicode)
 sanitized_diffName = invalid_chars_pattern.sub('_', diff_name)
 
 if data["resultsScreen"]["name"] == "":
-    print(f"No replay is selected. Quitting...")
-    time.sleep(1)
+    l.critical(f"No replay is selected. Quitting...")
+    time.sleep(2)
     sys.exit()
 else:
-    print(f"Replay of {username} on {map_name} loaded! Getting pp value...")
+    l.info(f"Replay of {username} on {map_name} loaded! Getting pp value...")
 
 ppData = requests.get(url=f"http://localhost:24050/api/calculate/pp?mode=0&mods={mods_num}&acc={play_accuracy}")
 
 if not ppData.status_code == 200:
-    print(f"Please check if either osu! is running or tosu is running correctly.")
+    l.error(f"Please check if either osu! is running or tosu is running correctly.")
 else:
     pp_Data = json.loads(ppData.content)
     playPp = pp_Data["pp"]
@@ -109,18 +132,19 @@ else:
 #extract player avatar
 if not os.path.exists('./players'):
     os.makedirs('./players')
-    print("Created a folder for player avatars at ./players")
+    l.info("Created a folder for player avatars at ./players")
 else:
-    print("Folder for player avatars already exists")
+    l.info("Folder for player avatars already exists")
 if not os.path.exists(f'./players/{username}.png'):
     user = f"https://osu.ppy.sh/api/get_user?k={userAPI}&u={username}"
     user_data = requests.get(user)
     if user_data.status_code == 200:
         a_data = user_data.json()
         userID = a_data[0]['user_id']
-        print(f"Collected avatar of {username}.")
+        l.info(f"Collected avatar of {username}.")
     else: 
-        print(f"There is something wrong, probably peppy got dunked.")
+       l.warning(f"""There is something wrong, probably peppy got dunked.
+                       Avatar will need to add manually.""")
 
         #fetching the avatar and download it
 
@@ -133,18 +157,21 @@ if not os.path.exists(f'./players/{username}.png'):
             a = add_rounded_corners(PlayerAvatar, radius=50).resize(size=(180, 180))
             a.convert("RGBA")
             a.save(f'./players/{username}.png')
-            print(f"Saved the avatar of {username} successfully.")
+            l.info(f"Saved the avatar of {username} successfully.")
     else:
-        print(f"There is something wrong, probably peppy got dunked.")
+       l.warning(f"""There is something wrong, probably peppy got dunked.
+                       Avatar will need to add manually.""")
 else:
-    print(f'Existed an image at ./players/{username}.png')
+    l.info(f'Existed an image at ./players/{username}.png')
 
 
 #creating the image
 background = Image.open("./statics/core.png")
+l.info("Loaded core.png")
 draw = ImageDraw.Draw(background)
 
-font_path = "./fonts/HvDTrial_FabrikatMono-Regular-BF64d5939342747.otf"
+default_font_path = "./fonts/FabrikatMono_Regular.otf"
+l.info("Loaded font")
 
 texts_fields = [
     {"type": "player", "text": f"{username}", "position": [244, 47.74], "font_size": 138.58},
@@ -153,7 +180,7 @@ texts_fields = [
     {"type": "beatmap_BPM", "text": f"{int(beatmap_BPM)}BPM", "position": [1765.51, 15], "font_size": 47},
     {"type": "mapper", "text": f"{mapper}", "position": [1575.1, 95.1], "font_size": 47},
     {"type": "star_rating", "text": f"{star_rating:.2f}*", "position": [1774.41, 433.46], "font_size": 61.34}, 
-    {"type": "c100", "text": f"{str(c100)}", "position": [70.11, 523.1], "font_size": 69.57},
+    {"type": "c100", "text": f"{str(c100)}", "position": [0, 0], "font_size": 69.57},
     {"type": "c50", "text": f"{str(c50)}", "position": [70.11, 655.51], "font_size": 69.57},
     {"type": "c0", "text": f"{str(c0)}", "position": [70.11, 790], "font_size": 69.57},
 ]
@@ -187,9 +214,9 @@ else:
 #handling pp
 formattedPp = int(playPp)
 if formattedPp < 1000:
-    texts_fields.append({"type": "pp", "text": f"{formattedPp}", "position": [400, 850], "font_size": 201})
+    texts_fields.append({"type": "pp", "text": f"{formattedPp}", "position": [550, 550], "font_size": 50})
 elif formattedPp > 1000:
-    texts_fields.append({"type": "pp", "text": f"{formattedPp}", "position": [360, 860], "font_size": 195})
+    texts_fields.append({"type": "pp", "text": f"{formattedPp}", "position": [612, 540], "font_size": 40})
 
 #handling grade
 grade_pos = [750, 230]
@@ -290,7 +317,7 @@ elif map_status == 7:
 #get the player avatar on screen
 playerAvatar = Image.open(f'./players/{username}.png')
 
-print(f"Loaded the player avatar")
+l.info(f"Loaded the player avatar")
 background.paste(playerAvatar, [35, 15], playerAvatar)
 
 #get the mods on the screen
@@ -352,10 +379,10 @@ texts_fields.append({"type": "map_Diff", "text": f"{diff_name}", "position": [16
 texts_fields.append({"type": "map_Name", "text": f"{map_name}", "position": [1600, 900], "font_size": font_size_name, "anchor": "mm"})
 #getting all da texts on screen
 for item in texts_fields:
-    font = ImageFont.truetype(font_path, item["font_size"])
+    font = ImageFont.truetype(item.get("font", default_font_path), item["font_size"])
     anchor_value = item.get("anchor", "la") 
     draw.text(item["position"], item["text"], font=font, fill="white", anchor=anchor_value, stroke_fill=(0,0,0), stroke_width=2.5)
-    print(", ".join('{}: {}'.format(key, val) for key, val in item.items()))
+    l.info(", ".join('{}: {}'.format(key, val) for key, val in item.items()))
 
 if not os.path.exists("./results"):
     os.makedirs(f"./results")
@@ -368,7 +395,7 @@ position = [(0,0), (0, 1080), (1200, 1080), (1450, 0)]
 # Open the image
 img = Image.open(background_true_path).convert("RGBA") # Ensure image has an alpha channel
 width, height = img.size
-print(width, height)
+l.debug(f"Background picture has {width}px x {height}px dimension")
 # Create a mask image with the same dimensions as the original image
 mask = Image.new('L', (width, height), 0) # 'L' for 8-bit pixels, 0 for black
 
@@ -395,7 +422,7 @@ output_img.paste(dimmed_img, (0, 0), inverted_mask)
 
 
 # Save the result
-output_img.save(f"./cache/{sanitized_title} croppedbackground.png") # <--- Changed output filename
+output_img.save(f"./cache/{sanitized_title} croppedbackground.png")
 
 background.save(f"./cache/{username} on {sanitized_title} [{sanitized_diffName}].png")
 
@@ -410,7 +437,7 @@ final_img.save(f"./results/{username} on {sanitized_title} [{sanitized_diffName}
 time.sleep(1)
 cache_folder_path = "./cache"
 if os.path.exists(cache_folder_path) and os.path.isdir(cache_folder_path):
-    print(f"Clearing contents of '{cache_folder_path}'...")
+    l.info(f"Clearing contents of '{cache_folder_path}'...")
     for item in os.listdir(cache_folder_path):
         item_path = os.path.join(cache_folder_path, item)
         try:
@@ -418,11 +445,11 @@ if os.path.exists(cache_folder_path) and os.path.isdir(cache_folder_path):
                 os.remove(item_path) # Remove files and symbolic links
             elif os.path.isdir(item_path):
                 shutil.rmtree(item_path) # Remove subdirectories and their contents
-            print(f"Removed: {item_path}")
+            l.info(f"Removed: {item_path}")
         except Exception as e:
-            print(f"Error removing {item_path}: {e}")
-    print("Cache folder contents cleared successfully.")
+            l.warning(f"Error removing {item_path}: {e}")
+    l.info("Cache folder contents cleared successfully.")
 elif not os.path.exists(cache_folder_path):
-    print(f"Cache folder '{cache_folder_path}' does not exist, no contents to clear.")
+    l.warning(f"Cache folder '{cache_folder_path}' does not exist, no contents to clear.")
 else:
-    print(f"'{cache_folder_path}' exists but is not a directory.")
+    l.warning(f"'{cache_folder_path}' exists but is not a directory.")
